522401232dbf904257447870adf9e7ebc479f66b
Eduardo Jirón <emjironal@gmail.com>
2020-02-17 16:52:33 -0600

    Asadas totales en admin/asadas y estudios/estadisticas

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -287,50 +287,6 @@ function comparar(){
     });
 };
 
-function getCanton(){
-	var parameters = { "provincia": document.getElementById("prov").value};
-	var cantones;
-	$.get('/getCantones',parameters,function(data) {
-		cantones = data;
-	   }).done(function(res){       
-		  var selectCant = document.getElementById("cant");
-		  selectCant.innerHTML="<option value='0'>Sin filtro</option>";
-		  cantones.rows.forEach(row => {
-			  selectCant.innerHTML+="<option value='"+row.ID+"'>"+row.Nombre+"</option>";
-		  });
-		  document.getElementById("dist").innerHTML="<option value='0'>Sin filtro</option>";
-	  });
-
-}
-
-
-function getDistrito(){
-	var parameters = { "provincia": document.getElementById("prov").value, "canton": document.getElementById("cant").value};
-	var distritos;
-	$.get('/getDistritos',parameters,function(data) {
-		distritos = data;
-	   }).done(function(res){       
-		  var selectCant = document.getElementById("dist");
-		  selectCant.innerHTML="<option value='0'>Sin filtro</option>";
-		  distritos.rows.forEach(row => {
-			  selectCant.innerHTML+="<option value='"+row.ID+"'>"+row.Nombre+"</option>";
-		  });
-	  });
-
-}
-
-function getAsada()
-{
-	var provincia = document.getElementById("prov");
-	var canton = document.getElementById("cant");
-	var distrito = document.getElementById("dist");
-	var asadas = document.getElementById("asada");
-	for(var i = 1; i < asadas.children.length; i++)
-	{
-		asadas[i].hidden = !(asadas[i].attributes.provincia.value == provincia.value && asadas[i].attributes.canton.value == canton.value && asadas[i].attributes.distrito.value == distrito.value);
-	}
-}
-
 function getEstadisticas(){
 	var parameters = { "provincia": document.getElementById("prov").value, "canton": document.getElementById("cant").value, "distrito": document.getElementById("dist").value, "orden": document.getElementById("ord").value};
 	var distritos;
@@ -339,6 +295,8 @@ function getEstadisticas(){
 	   }).done(function(res){       
 		  var selectCant = document.getElementById("componenttable");
 		  selectCant.innerHTML="";
+		  var totalAsadas = document.getElementById("totalAsadas");
+		  totalAsadas.textContent = "Total de ASADAS: " + distritos.rows.length;
 		  distritos.rows.forEach(consulta => {
 			  selectCant.innerHTML+="<tr><td>"+consulta.Asada_ID+"</td><td>"+consulta.Nombre+"</td><td>"+
 			  ""+consulta.Distrito+"</td><td>"+consulta.Canton+"</td><td>"+consulta.Provincia+"</td><td>"+
