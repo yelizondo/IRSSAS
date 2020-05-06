40138d6a67574506d7ee95612481520e0d69d053
Nahomy Moya <nahomymoya@pop-os.localdomain>
2020-05-05 21:11:55 -0600

    Caso 17: Ayuda de Riesgos en el Mapa

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 8000
 
 //llamar funciones de controller.js
 
-const {getCrudComponente, saveComponente, getCrudSubcomponente, saveSubComponente, getCrudIndicador, getIndicador, deleteIndicador, updateIndicador, newIndicador, createIndicador, getCrudAsadasR,getCrudAsadasU, getPresentAsada, saveAsada, newAsada, createAsada, deleteAsada, crudFormularios, sendForm, getCrudUsuario, saveUsuario, getUsuariosAsadas,setUsuariosAsada, guardarFormulario, cargarFormulario, getContacto, updateEstado, changePassword,forgetPassword, getListaAsociaciones, nuevaAsociacion, nuevaAsociacionGuardar, editarAsociacion, editarAsociacionGuardar, eliminarAsociacion, sendSolicitudRegistroAsada, aceptarRechazarSolicitudRegistroAsada, getAyudaPregunta, deleteNotificacion} = require('./routes/cruds');
+const {getCrudComponente, saveComponente, getCrudSubcomponente, saveSubComponente, getCrudIndicador, getIndicador, deleteIndicador, updateIndicador, newIndicador, createIndicador, getCrudAsadasR,getCrudAsadasU, getPresentAsada, saveAsada, newAsada, createAsada, deleteAsada, crudFormularios, sendForm, getCrudUsuario, saveUsuario, getUsuariosAsadas,setUsuariosAsada, guardarFormulario, cargarFormulario, getContacto, updateEstado, changePassword,forgetPassword, getListaAsociaciones, nuevaAsociacion, nuevaAsociacionGuardar, editarAsociacion, editarAsociacionGuardar, eliminarAsociacion, sendSolicitudRegistroAsada, aceptarRechazarSolicitudRegistroAsada, getAyudaPregunta, deleteNotificacion, getAyudaRiesgo} = require('./routes/cruds');
 const {getHomePage, login, getMain, getVisor, getComponente, logout, getSites, grafico, getRiesgo, getAsada, getInfoGeneral, generarInforme, histFormulario, getAnno, getRespuestas, comparaMapas, statsComponentes,statsSubcomponentes, getCantones, getDistritos, getEstadisticas, getMapa, getManualData, getManualUsuario, getManualDataDescargar, getRutas, getRutasData, solicitudRegistroAsada, validarUsuario, getVerSolicitudRegistroAsada,getEstadisticasGenerales} = require('./routes/controller');
 
 //conexion de BD
@@ -149,7 +149,8 @@ app.get('/verSolicitudRegistroAsada', getVerSolicitudRegistroAsada);
 app.post('/verSolicitudRegistroAsada/respuesta', aceptarRechazarSolicitudRegistroAsada);
 app.get('/getAyudaPregunta', getAyudaPregunta);
 app.post('/deleteNotificacion', deleteNotificacion);
-app.get('/verEstadisticasGenerales', getEstadisticasGenerales)
+app.get('/verEstadisticasGenerales', getEstadisticasGenerales);
+app.get('/getAyudaRiesgo/:idRiesgo', getAyudaRiesgo);
 
 // llamada al puerto 
 app.listen(PORT, () => {
