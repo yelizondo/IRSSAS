6a862c9d28084df432b26410d30ec7b15e4df71e
Eduardo Jirón <emjironal@gmail.com>
2020-06-15 21:53:57 -0600

    [fixed] Contacto ASADA

--- routes/cruds.js
+++ routes/cruds.js
@@ -790,7 +790,7 @@ module.exports = {
     }, //end cargarFormulario
 
     getContacto: (req, res) => {
-        let query = "select a.ID,a.Nombre,p.ID as Provincia,c.ID as Canton,d.ID as Distrito,ai.Ubicacion,ai.Telefono from ASADA a left join ASADAINFO ai on a.ID=ai.Asada_ID inner join DISTRITO d on a.distrito_id=d.Codigo inner join CANTON c on d.Canton_ID=c.ID inner join PROVINCIA p on p.ID=c.Provincia_ID where d.Provincia_ID=p.ID order by Provincia ASC, Canton ASC, Distrito ASC, a.Nombre ASC;";
+        let query = "select a.ID,a.Nombre,p.ID as Provincia,c.ID as Canton,d.Codigo as Distrito,ai.Ubicacion,ai.Telefono from ASADA a left join ASADAINFO ai on a.ID=ai.Asada_ID inner join DISTRITO d on a.distrito_id=d.Codigo inner join CANTON c on d.Canton_ID=c.ID inner join PROVINCIA p on p.ID=c.Provincia_ID where d.Provincia_ID=p.ID order by Provincia ASC, Canton ASC, Distrito ASC, a.Nombre ASC;";
         let query2 = "select ID, Nombre from PROVINCIA";
         let query3 = "select ID, Nombre from CANTON";
         let query4 = "select ID, Nombre from DISTRITO";
